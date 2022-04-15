9d8dc2ed453f0120d24a1d8ba6094a39f46bd8aa
Davide Graceffa <davide.graceffa@trilog.it>
2022-04-15 15:39:11 +0200

    feat(server.js): mongoDb start

--- server.js
+++ server.js
@@ -1,12 +1,20 @@
-require('./models/db');
+// require('./models/db');
 
 const express = require('express');
 
-const employeeController = require ('./controllers/employeeController')
+// const employeeController = require('./controllers/employeeController');
 var app = express();
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost:27017/EmployeeDB', { useNewUrlParser: true }, (err) => {
+    if (!err) { console.log('MongoDB Connection Succeeded.') }
+    else { console.log('Error in DB connection : ' + err) }
+});
 
 app.listen(3000, () => {
-    console.log('Express server started on port : 3000');
-})
+  console.log(
+    'Express server started on port : 3000'
+  );
+});
 
-app.use('/employee', employeeController)
+// app.use('/employee', employeeController);
